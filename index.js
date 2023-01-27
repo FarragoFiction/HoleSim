a559efc0c451435d478ed75249a2bc261e4f55d2
JR <jadedResearcher@gmail.com>
2023-01-26 23:22:36 -0500

    less efficient rendering but lets me get collision detection

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ let holeLevel = 0;
 let hole;
 const maxHoleLevel = 25;
 let started = false;
-const audioSources = ["audio/651292__f3bbbo__digging-in-wet-course-sand-1.mp3","audio/651293__f3bbbo__digging-in-wet-course-sand-2.mp3"];
+const audioSources = ["audio/651292__f3bbbo__digging-in-wet-course-sand-1.mp3", "audio/651293__f3bbbo__digging-in-wet-course-sand-2.mp3"];
 window.onload = () => {
   hole = document.querySelector("#hole");
 
   audio.play();
   window.onclick = () => {
     const splash = document.querySelector("#splash");
-    if(splash.style.display != "none"){
+    if (splash.style.display != "none") {
       splash.style.display = "none";
       const bg = document.querySelector("#fake-background");
       bg.style.filter = "brightness(1.0)";
@@ -21,13 +21,13 @@ window.onload = () => {
     }
 
     const audio = document.querySelector("#audio");
-    if(!started){
+    if (!started) {
       started = true;
       start();
 
-    }else{
-      if(holeLevel<maxHoleLevel){
-       oneBadImpulse();
+    } else {
+      if (holeLevel < maxHoleLevel) {
+        oneBadImpulse();
       }
     }
 
@@ -40,51 +40,52 @@ window.onload = () => {
 
 }
 
-const collisionDectionLoop = ()=>{
+
+const collisionDectionLoop = () => {
   //good or bad
   let impulses = document.querySelectorAll(".impulse");
   let holeRect = hole.getClientRects()[0];
-  for(let impulse of impulses){
+  for (let impulse of impulses) {
     //does the middle of the impulse touch any part of the hole?
     let impulseRect = impulse.getClientRects()[0]; //get rect
-    const middleX = impulseRect.x + impulseRect.width/2;
-    const middleY = impulseRect.y + impulseRect.height/2;
+    const middleX = impulseRect.x + impulseRect.width / 2;
+    const middleY = impulseRect.y + impulseRect.height / 2;
     //console.log("JR NOTE: debug: ",{middleX, middleY, holeX: holeRect.x, holeY: holeRect.y, holeWidth: holeRect.width, holeHeight: holeRect.height})
-        //wiggle around the middle. 
+    //wiggle around the middle. 
 
-    const wiggleWidth = impulseRect.width/4;
-    const wiggleHeight = impulseRect.height/4;
-    const middleHorizontal = ()=>{
+    const wiggleWidth = impulseRect.width / 2;
+    const wiggleHeight = impulseRect.height / 2;
+    const middleHorizontal = () => {
 
-      if( middleX+wiggleWidth > holeRect.x && middleX+wiggleWidth < holeRect.x + holeRect.width){
+      if (middleX + wiggleWidth > holeRect.x && middleX + wiggleWidth < holeRect.x + holeRect.width) {
         return true;
       }
 
-      if( middleX-wiggleWidth > holeRect.x && middleX-wiggleWidth < holeRect.x + holeRect.width){
-        return true;
-      }
-      return false;
-    }
-
-    const middleVertical = ()=>{
-      if( middleY+wiggleHeight > holeRect.y && middleY+wiggleHeight < holeRect.y + holeRect.height){
-        return true;
-      }
-
-      if( middleY-wiggleHeight > holeRect.y && middleY-wiggleHeight < holeRect.height + holeRect.height){
+      if (middleX - wiggleWidth > holeRect.x && middleX - wiggleWidth < holeRect.x + holeRect.width) {
         return true;
       }
       return false;
     }
 
-    if(middleHorizontal() && middleVertical()){
+    const middleVertical = () => {
+      if (middleY + wiggleHeight > holeRect.y && middleY + wiggleHeight < holeRect.y + holeRect.height) {
+        return true;
+      }
+
+      if (middleY - wiggleHeight > holeRect.y && middleY - wiggleHeight < holeRect.height + holeRect.height) {
+        return true;
+      }
+      return false;
+    }
+
+    if (middleHorizontal() && middleVertical()) {
       impulse.remove();
       embiggenHole();
       oneBadImpulse();
     }
   }
-  if(holeLevel < maxHoleLevel){
-    setTimeout(collisionDectionLoop, 50)
+  if (holeLevel < maxHoleLevel) {
+    setTimeout(collisionDectionLoop, 200)
   }
 }
 
@@ -94,32 +95,32 @@ and slowly showing that even if he refuses to acknowledge his past
 even if he keeps it buried
 he can still find healing
 */
-const start = ()=>{
+const start = () => {
   oneBadImpulse();
   collisionDectionLoop();
 
 }
 
-const embiggenHole = ()=>{
+const embiggenHole = () => {
   const audio = new Audio(pickFrom(audioSources));
   audio.play();
-  holeLevel ++;
-  console.log("JR NOTE: trying to embiggen ",hole.style.width)
-  hole.style.width = `${5+holeLevel}vw`
-  console.log("JR NOTE: trying to embiggen ",hole.style.width)
+  holeLevel++;
+  console.log("JR NOTE: trying to embiggen ", hole.style.width)
+  hole.style.width = `${5 + holeLevel}vw`
+  console.log("JR NOTE: trying to embiggen ", hole.style.width)
   const body = document.querySelector("body");
-  body.style.filter=`brightness(${1-holeLevel/30})`;
+  body.style.filter = `brightness(${1 - holeLevel / 30})`;
 
 
 }
 
 //lol bro, you have to wander eternity obsessed with something meaningless that can neither sate you nor end, get pranked
 
-const oneBadImpulse = ()=>{
-  let ele = createElementWithClass("div","bad-impulse");
+const oneBadImpulse = () => {
+  let ele = createElementWithClass("div", "bad-impulse");
   ele.innerText = "JR TEST";
-  bounceTime(ele);
-  if(holeLevel<maxHoleLevel){
+  bounceTime(ele, false);
+  if (holeLevel < maxHoleLevel) {
     setTimeout(oneBadImpulse, 10000)
   }
 
@@ -148,9 +149,101 @@ I want to be excited that we’re seeing more and more what lies behind our own 
 */
 
 
+const debugTime = (ele) => {
+  console.log("JR NOTE: debug time")
+  const container = document.querySelector("body");
+  const offset = 25;
+  if (container && ele) {
+    console.log("JR NOTE: gonna set up a mouse move on", container, ele)
+    container.onmousemove = (event) => {
+      console.log(event);
+      ele.style.top = `${event.offsetY + offset}px`;
+      ele.style.left = `${event.offsetX + offset}px`;
+    }
+  }
+}
+
+//https://stackoverflow.com/questions/8897289/how-to-check-if-an-element-is-off-screen
+const eleIsOffScreen = (el) => {
+  const rect = el.getBoundingClientRect();
+  return (
+    (rect.x + rect.width) < 0
+    || (rect.y + rect.height) < 0
+    || (rect.x > window.innerWidth || rect.y > window.innerHeight)
+  );
+}
+
+const eleIsOffScreenX = (el) => {
+  const rect = el.getBoundingClientRect();
+  return ((rect.x + rect.width) < 0 || (rect.x > window.innerWidth));
+}
+
+const eleIsOffScreenY = (el) => {
+  const rect = el.getBoundingClientRect();
+  return ( (rect.y + rect.height) < 0 || rect.y > window.innerHeight)
+}
+
+const bounceTime = (ele, debug) => {
+  if (!bounce_container) {
+    bounce_container = document.querySelector("#bounce-container");
+  }
+  const elWrap = createElementWithClassAndParent("div", bounce_container, `el-wrap impulse`);
+  const el = createElementWithClassAndParent("div", elWrap, `el`);
+  el.style.width = "50px";
+  el.style.height = "50px";
+
+  el.append(ele);
+  if (debug) {
+    debugTime(elWrap);
+  } else {
+    elWrap.style.left = `${getRandomNumberBetween(0, window.innerWidth-100)}px`;
+    elWrap.style.top = `${getRandomNumberBetween(0, window.innerHeight-100)}px`;
+  }
+
+  let xDirection = pickFrom([1, -1]);
+  let yDirection = pickFrom([1, -1]);
+  let xSpeed = getRandomNumberBetween(10, 30)
+  let ySpeed = getRandomNumberBetween(10, 30)
+
+  //lol this is so un-functional. so many side effects. catharsis
+  const resetVectorsX = () => {
+    xDirection = xDirection * -1;
+    yDirection = pickFrom([1, -1]);
+    xSpeed = getRandomNumberBetween(10, 30)
+    ySpeed = getRandomNumberBetween(10, 30)
+  }
+
+  const resetVectorsY = () => {
+    xDirection = pickFrom([1, -1]);
+    yDirection = yDirection * -1;
+    xSpeed = getRandomNumberBetween(10, 30)
+    ySpeed = getRandomNumberBetween(10, 30)
+  }
+
+
+  const bounce = () => {
+    if (eleIsOffScreenX(elWrap)) {
+      resetVectorsX();
+    }
+    //NOT else, can be off screen in a corner
+
+    if (eleIsOffScreenY(elWrap)) {
+      resetVectorsY();
+    }
+    elWrap.style.top = `${parseInt(elWrap.style.top) + 10 * yDirection}px`;
+    elWrap.style.left = `${parseInt(elWrap.style.left) + 10 * xDirection}px`;
+    setTimeout(() => { requestAnimationFrame(bounce) }, 50)
+
+
+  }
+  bounce();
+
+}
+
 
 //https://css-tricks.com/bounce-element-around-viewport-in-css/
-const bounceTime = (ele) => {
+//:( :( :( this doens't work with collision detection)))
+const bounceTimeOld = (ele, debug) => {
   //multiple things we wanna do. first is just bounce it around as is
   //then give it three frames of animation (same as LOGAC) that makes it staticky
   if (!bounce_container) {
@@ -162,11 +255,14 @@ const bounceTime = (ele) => {
   </div>
   */
   //ternary is so i can debug it without it zipping about
-  const xAnimations = false ? ["x-turtle"] : ["x", "x-fast"];
-  const yAnimations = false ? ["y-turtle"] : ["y", "y-fast"];
+  const xAnimations = debug ? [""] : ["x", "x-fast"];
+  const yAnimations = debug ? [""] : ["y", "y-fast"];
   const elWrap = createElementWithClassAndParent("div", bounce_container, `el-wrap ${pickFrom(xAnimations)} impulse`);
-  elWrap.style.left = `${getRandomNumberBetween(0, 50)}vw`;
-  elWrap.style.top = `${getRandomNumberBetween(0, 50)}vh`;
+
+  if (!debug) {
+    elWrap.style.left = `${getRandomNumberBetween(0, 50)}vw`;
+    elWrap.style.top = `${getRandomNumberBetween(0, 50)}vh`;
+  }
   //elWrap.style.left = `50vw`; (hole location)
   //elWrap.style.top = `54vh`;
   const el = createElementWithClassAndParent("div", elWrap, `el ${pickFrom(yAnimations)}`);
@@ -174,6 +270,9 @@ const bounceTime = (ele) => {
   el.style.height = "50px";
 
   el.append(ele);
+  if (debug) {
+    debugTime(elWrap);
+  }
 
 }
 
